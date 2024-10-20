88aa976e53a1d0087c6208857d19d0aa43f37046
Shubham Jaiswal <85643876+shubhamjaiswal04@users.noreply.github.com>
2024-10-20 20:09:03 +0000

    conversion of data types

---
+++ 01_basics/03_coversionOperation.js
@@ -0,0 +1,34 @@
+//let score= "33"
+let score= "33abc"
+
+//console.log(typeof score);
+//console.log(typeof (score));
+
+let valueInNumber= Number(score)
+//console.log(typeof valueInNumber);
+//console.log(valueInNumber);
+
+
+// "33" =>33
+// "33abc" => NaN
+//true =>1; false=>0
+
+
+//let isLoggedIn="Shubu"
+//let isLoggedIn=1
+let isLoggedIn=""
+
+let booleanIsLoogedIn=Boolean(isLoggedIn)
+console.log(booleanIsLoogedIn)
+
+//1 => true; 0=> false
+//"" => false
+//"Shubu" => true
+
+
+let someNumber=33
+
+let stringNumber=String(someNumber)
+
+console.log(stringNumber)
+console.log( typeof stringNumber)
