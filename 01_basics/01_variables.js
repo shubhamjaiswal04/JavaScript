c0c817465c42b2e25d07481453e5668c2f863d55
Shubham Jaiswal <85643876+shubhamjaiswal04@users.noreply.github.com>
2024-10-20 19:51:46 +0000

    variables

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,17 @@
+const accountId=14453
+let accountEmail="liveshubam.cs@gmail.com"
+var accountPassword="123445"
+accountCity="Noida"
+
+//accountId=2    //not allowed to chnage const value
+
+accountEmail="shubham@gmail.com"
+accountPassword="122132"
+accountCity="Gurugram"
+let status;
+console.log(accountId);
+/*
+prefferd not to use of var
+because of issue in block scope and functional scope
+*/
+console.table([accountId,accountEmail,accountPassword,accountCity,status])
